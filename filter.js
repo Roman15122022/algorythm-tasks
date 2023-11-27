acc25df6f336d11f39b7fda4dca220eb1c8edf53
Roman15122022 <mallrom1231@gmail.com>
2023-11-27 11:14:26 +0200

    feat: filter

---
+++ filter.js
@@ -0,0 +1,11 @@
+const filter = function(arr, fn) {
+    let answer = [];
+    for(let i = 0; i < arr.length; i++){
+        if(fn(arr[i], i)){
+            answer.push(arr[i]);
+        }
+    }
+    return answer;
+};
+
+console.log(filter([1,2,6,7,8,], (x) => x>5))
