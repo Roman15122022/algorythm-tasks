97d77c0553907032190a028cfa1d1c781a41376f
Roman15122022 <mallrom1231@gmail.com>
2023-11-29 14:46:57 +0200

    feat: flatten

---
+++ flatten.js
@@ -0,0 +1,15 @@
+
+function flatten(arr){
+    let finallyArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if (Array.isArray(arr[i])){
+            const flattenArr = flatten(arr[i]);
+            finallyArr.push(...flattenArr);
+        }else {
+            finallyArr.push(arr[i]);
+        }
+    }
+    return finallyArr;
+}
+
+console.log(flatten([1,2,3,[1,4],[[1,2,3]]])) // 123,1,2,3,3
