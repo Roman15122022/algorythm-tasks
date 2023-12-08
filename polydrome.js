0479c34fe1ec3d078cb71452a564f9f5dcb4f296
Roman15122022 <mallrom1231@gmail.com>
2023-12-08 03:30:28 +0200

    chore: polyndrome

--- polydrome.js
+++ polydrome.js
@@ -3,7 +3,7 @@
 let str = 'deed';
 let str2 = 'uаasddsaаu'
 
-const checkOnPolydrome = (str) =>{  //O(n/2) == O(n) ;
+const checkOnPolydrome = (str) =>{  //O(n/2) == O(n);
     str = str.toLowerCase();
     for (let i = 0; i < str.length/2; i++){
         if (str[i] !== str[--str.length-i]){
