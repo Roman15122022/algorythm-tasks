26a58c43ffcba11a0ed26293fb02c288b65206d9
Roman15122022 <mallrom1231@gmail.com>
2023-12-10 01:49:46 +0200

    chore: chunk

--- chunk.js
+++ chunk.js
@@ -4,5 +4,22 @@
 
 const arr = [1, 2, 3, 4, 5, 6];
 const chunk = (arr, num) => {
+    let result = [];
+    let chunk = [];
+    let counter = 1;
+
+    arr.forEach((item) =>{
+        chunk.push(item);
+        if (counter === num){
+            result.push(chunk);
+            chunk = [];
+            counter = 0;
+        }
+        counter++;
+    })
+    if (chunk.length) result = [result, [... chunk]];
+    return result;
 
 }
+
+console.log(chunk(arr,4));
