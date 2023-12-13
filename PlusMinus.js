8bc5989d8950cf6017d1da9ec301331a14f0f60e
Roman15122022 <mallrom1231@gmail.com>
2023-12-14 01:07:20 +0200

    feat: PlusMinus

---
+++ PlusMinus.js
@@ -0,0 +1,10 @@
+// 2.plus(3).minus.(1) = 4;
+
+Number.prototype.plus = function (num) {
+    return this + num;
+}
+Number.prototype.minus = function (num) {
+    return this - num;
+}
+
+console.log((2).plus(3).minus(1));
