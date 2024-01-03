8aabfdc7c36cdf563a03467ba9d276eb3da19278
Roman15122022 <120688434+Roman15122022@users.noreply.github.com>
2024-01-04 01:46:27 +0200

    feat: polyndrome + answer

--- polydrome.js
+++ polydrome.js
@@ -3,20 +3,29 @@
 let str = 'deed';
 let str2 = 'uаasddsaаu'
 
-const checkOnPolydrome = (str) =>{  //O(n/2) == O(n);
+const checkOnPolydrome = (str) => {  //O(n/2) == O(n);
     str = str.toLowerCase();
-    for (let i = 0; i < str.length/2; i++){
-        if (str[i] !== str[--str.length-i]){
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[--str.length - i]) {
             return false;
         }
     }
     return true;
 }
 
-const polyndrome = (str) =>{
+const polyndrome = (str) => {
     let string = str.toLowerCase();
     return (string === string.split('').reverse().join(''));
 }
 
+const polyndrome2 = (str) => {
+    let string = '';
+    for (let i = str.length-1; i >= 0; i--) {
+        string += str[i];
+    }
+    return string === str;
+}
+
 console.log(checkOnPolydrome(str2))
 console.log(polyndrome(str2))
+console.log(polyndrome2(str2))
