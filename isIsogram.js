55046f2004535a15350c8beb4a260145718fcd34
Roman15122022 <120688434+Roman15122022@users.noreply.github.com>
2024-01-08 00:10:11 +0200

    fix: isIsogram

--- isIsogram.js
+++ isIsogram.js
@@ -3,17 +3,25 @@
 // isIsogram "aba" = false
 
 const isIsogram = (str) => {
+    if (!str.length) return true;
+
+    const lowerStr = str.toLowerCase();
+
     const letter = {};
-    for (let i = 0; i < str.length; i++) {
-        letter[str[i]] = (letter[str[i]] || 0) + 1;
+
+    for (let i = 0; i < lowerStr.length; i++) {
+        letter[lowerStr[i]] = (letter[lowerStr[i]] || 0) + 1;
     }
+
     const arr = Object.values(letter);
+
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] >= 2){
+        if (arr[i] >= 2) {
             return false;
         }
     }
+
     return true;
 }
 
-console.log(isIsogram('moose'));
+console.log(isIsogram('moOse'));
