99d62e110490917ae793ee05b0d19d581bd8671f
Roman15122022 <120688434+Roman15122022@users.noreply.github.com>
2024-02-09 00:38:33 +0200

    feat: groupByRemainder.js

---
+++ groupByRemainder.js
@@ -0,0 +1,13 @@
+const numbers = [15, 21, 30, 45, 17, 22, 36, 41, 57];
+const divisor = 5;
+
+
+const groupByRemainder = (numbers, divisor) => {
+    return numbers.reduce((memo, item) => {
+        const rest = item % divisor;
+        memo[rest] = [...memo[rest] || [], item];
+        return memo;
+    }, {})
+}
+
+console.log(groupByRemainder(numbers, divisor))
